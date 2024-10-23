8e07d5fa5927503cb3a02e97b257a792e1404fd6
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-23 11:29:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	let charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz"
+
+	for(let i = 0; i < l; i++){
+		res += charList.charAt(
+			Math.floor(Math.random * charList.length);
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
